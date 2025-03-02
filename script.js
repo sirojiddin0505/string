9bf01e3df211ffc24a30ee577b325efb6a751453
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-03-02 10:56:46 +0500

    2commit

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 // uyga vazifa
 // 1
+
+// function alphabet(n) {
+//     if (n < 1 || n > 26) {
+//         console.log("n 1 dan 26 gacha bo'lishi kerak.");
+//         return;
+//     }
+
+//     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     let result = alphabet.slice(0, n); 
+//     console.log(result); 
+// }
+// alphabet(1);
+
+
+
+// 2  ?
+
+
+// 3
+// let x = "javascript"
+// console.log(x.slice(0,1), "", x.slice(9,10));
+
+
+// 4
+// function repeats(n, item){
+//     let result = item.repeat(n);
+//     console.log(result);
+    
+// }repeats(7, "A")
+
+
+
+// 5
+
